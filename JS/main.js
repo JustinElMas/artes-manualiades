d780ef8f1a2d30f6819da53d9ad88ef9f1e838b9
justinyemiliano <justin7bjjr2014@gmail.com>
2022-08-31 07:02:35 -0500

    Add the images on the section categories

--- JS/main.js
+++ JS/main.js
@@ -5,3 +5,5 @@ const mainMenuelement = document.getElementById('main-menu');
 toggleMenuelement.addEventListener('click', () => {
     mainMenuelement.classList.toggle('main-menu--show');
 })
+
+// una sección de cursos permanentes, la bolita quitar detalles poner cursos permanentes, quitar dinero, poner descipción de lo que es.
