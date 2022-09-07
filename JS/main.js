b81460797ec24a2867e502cc730c37cd3bfc5d9c
justinyemiliano <justin7bjjr2014@gmail.com>
2022-09-07 09:52:05 -0500

    Add the styles and the navbar-mode and types courses

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,15 @@
 
 const toggleMenuelement = document.getElementById('toggle-menu');
 const mainMenuelement = document.getElementById('main-menu');
+const navAltura = document.getElementById("altura");
+const marginTop = document.getElementById("top-margin");
 
 toggleMenuelement.addEventListener('click', () => {
     mainMenuelement.classList.toggle('main-menu--show');
+    navAltura.classList.toggle('altura-nav');
+    marginTop.classList.toggle('margentop');
 })
+
+
 
 // una sección de cursos permanentes, la bolita quitar detalles poner cursos permanentes, quitar dinero, poner descipción de lo que es.
