5f85e50a66bf0590a5cb4c8e83b297f37ac1c500
justinyemiliano <justin7bjjr2014@gmail.com>
2022-09-14 14:04:18 -0500

    Add the adress corretion

--- JS/main.js
+++ JS/main.js
@@ -13,3 +13,5 @@ toggleMenuelement.addEventListener('click', () => {
 
 
 // una sección de cursos permanentes, la bolita quitar detalles poner cursos permanentes, quitar dinero, poner descipción de lo que es.
+// ¿cómo se inscriben desde su casa?
+// (una idea es incorporar un menú hamburguesa que contenga las fotos) añadir las fotos para las cursos pero de una forma épica y distinta que incorpore un buen diseño (dos para ca uno), con responsive disign
