e2b17ec5bbb8a4607c4e277959c692ebe511d3a8
justinyemiliano <justin7bjjr2014@gmail.com>
2022-09-21 16:03:59 -0500

    A retocate ultimate because we go a send to the hosting

---
+++ JS/cookies.js
@@ -0,0 +1,24 @@
+const botonAceptarCookies = document.getElementById("btn-aceptar-cookies");
+const avisoCookies = document.getElementById("aviso-cookies");
+const fondoAvisoCookies = document.getElementById("fondo-ads-cookies");
+
+dataLayer = [];
+
+if (!localStorage.getItem("cookies-aceptadas", true)) {
+    avisoCookies.classList.add("activo");
+    fondoAvisoCookies.classList.add("activo");
+} else {
+    dataLayer.push({'event': 'aceptaporfa'})
+};
+
+
+botonAceptarCookies.addEventListener("click", () => {
+    avisoCookies.classList.remove("activo");
+    fondoAvisoCookies.classList.remove("activo");
+
+    localStorage.setItem("cookies-aceptadas", true);
+    console.log("El usuario aceptó las cookies");
+
+    dataLayer.push({'event': 'aceptaporfa'})
+    
+});
